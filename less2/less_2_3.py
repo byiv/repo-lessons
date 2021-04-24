a859b6adb997ae798254e9235c0b079a705b9163
byiv <b.ivan.s85@gmail.com>
2021-04-24 08:32:51 +0300

    less_2_3 dict

--- less2/less_2_3.py
+++ less2/less_2_3.py
@@ -6,7 +6,8 @@
 
 month_num = int(input('Введите номер месяца: '))
 month_list = [[3, 4, 5], [6, 7, 8], [9, 10, 11], [12, 1, 2]]
-month_dict = {(3, 4, 5): 'Весна', (6, 7, 8): 'Лето', (9, 10, 11): 'Осень', (12, 1, 2): 'Зима'}
+month_dict = {3: 'Весна', 4: 'Весна', 5: 'Весна', 6: 'Лето', 7: 'Лето', 8: 'Лето',
+              9: 'Осень', 10: 'Осень', 11: 'Осень', 12: 'Зима', 1: 'Зима', 2: 'Зима'}
 
 if month_num in month_list[0]:
     print('Весна')
@@ -18,4 +19,4 @@ else:
     print('Зима')
 
 # print(month_dict.get(month_dict.keys()))
-print(month_dict.keys(month_num))
+print(month_dict.get(month_num))
